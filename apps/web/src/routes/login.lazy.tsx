3001e5726736007242548c02492ff86b2f27bd96
bangwu <119404476+markbang@users.noreply.github.com>
2026-03-06 13:54:01 +0800

    perf(web): optimize bootstrap loading (#6)

---
+++ apps/web/src/routes/login.lazy.tsx
@@ -0,0 +1,18 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import SignInForm from "@/components/sign-in-form";
+import SignUpForm from "@/components/sign-up-form";
+
+export const Route = createLazyFileRoute("/login")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	const [showSignIn, setShowSignIn] = useState(false);
+
+	return showSignIn ? (
+		<SignInForm onSwitchToSignUp={() => setShowSignIn(false)} />
+	) : (
+		<SignUpForm onSwitchToSignIn={() => setShowSignIn(true)} />
+	);
+}
